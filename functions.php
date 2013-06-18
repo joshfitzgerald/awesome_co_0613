a037a420da252bbb3b4b1194113628dea7dd7cb9
Melissa Cabral <melissacabral@gmail.com>
2013-06-18 07:51:46 -0700

    first version

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+//unlock sleeping features
+add_theme_support( 'post-thumbnails' );
+
+//set up custom image sizes
+//name, width, height, crop (bool)
+add_image_size( 'awesome-home-banner', 960, 330, true );
+
+
+//DO NOT CLOSE PHP
